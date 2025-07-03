4b4b15f3f02eede84ba0aee0603813768f7d3571
Gateway Software SolutionS <developmentgateway111@gmail.com>
2025-07-03 12:54:53 +0530

    Initial commit of PlayArena Flutter project

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,6 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-// 🔐 Providers
-
-
 // 🎨 Theme
 import ' providers/auth_provider.dart';
 import ' providers/locale_provider.dart';
